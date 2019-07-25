0848602359a110edb7e664033c52ae5dee995898
fireyy <fireyy@gmail.com>
2019-07-25 11:22:46 +0800

    clean

--- content/blog/2011/play.md
+++
@@ -1,8 +0,0 @@
------------
-title: 开一个playground
-date: 2011-11-24
-folder: blog
-tags: life, play
------------
-
-之后在这里发各种play吧！
--- content/twitter/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include('twitter.php');
-$obj = new GetTweetsFrom("fireyy",5,true);
-echo '<div class="twitter content">'.$obj.'</div>'; 
-  
-?>
