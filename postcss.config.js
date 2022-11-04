1dfbca9f497bf1b2498e287a1f4d9e42222da4c8
fireyy <fireyy@gmail.com>
2022-11-04 17:29:49 +0800

    clean

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const atImport = require('postcss-import')
-const nano = require('cssnano')
-
-module.exports = {
-  plugins: [
-    atImport(),
-    nano({ preset: 'default' })
-  ]
-}
--- src/utils/formatDate.js
+++
@@ -1,5 +0,0 @@
-const format = date => date.toLocaleDateString('en-US', {year: "numeric", month: "short", day: "numeric"})
-
-export default date => {
-  return (date instanceof Date) ? format(date) : format(new Date(date))
-}
